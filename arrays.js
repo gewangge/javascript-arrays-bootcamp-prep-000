ed2a0a343aaa59c86472d5152ba39b68a5489b44
Ge Wang <wg.wangge1984@gmail.com>
2018-02-21 21:25:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (array, string) {
-    array.unshift(string)
-    return array
+    return  [string, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray (array, string) {
-  return  [string, ...array]
+  array.unshift(string)
+  return array
 }
